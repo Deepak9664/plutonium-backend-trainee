90bf6621b534e6e4c150bcda46f0ad9f90e82d4e
deepak9664 <singhdepak30@gmail.com>
2022-08-08 18:16:00 +0530

    Api

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,45 +1,38 @@
 const express = require('express');
 const abc = require('../introduction/intro')
-//  const assign = require('../logger/logger')
-//  const assign1 = require('../util/helper')
-//  const assign2 = require('../validator/formatter')
-const Lodash = require('lodash');
 const router = express.Router();
 
-router.get('/test-me', function (req, res) {
-    console.log('My batch is plutonium', )
-    // assign.Cuh()
-    // assign1.F1()
-    // assign1.F2()
-    // assign1.F3()
-    // assign2.Trim()
-    // assign2.Smallalpha()
-    // assign2.Bigalpha()
-    const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul","Aug", "Sep", "Oct", "Nov", "Dec"];
-    const split = Lodash.chunk(months,4);
-    console.log(split);
-    const Odd = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19];
-    const Tail = Lodash.tail(Odd)
-    console.log(Tail);
-    const Arrayyy = Lodash.union([1,2,3],[3,4,5],[4,5,6],[5,6,7],[7,8,9]);
-    console.log(Arrayyy);
-    const Pairs = [["horror","The Shining"],["drama","Titanic"],["thriller","Shutter Island"],["fantasy","Pans Labyrinth"]]
-    const keypairs = Lodash.fromPairs(Pairs);
-    console.log(keypairs);
+// router.get('/test-me', function (req, res) {
+//     console.log('My batch is', abc.name)
+//     abc.printName()
+//     logger.welcome()
 
-    res.send('My second ever api!')
-});
+    // res.send('My second ever api!')
+// });
 
+router.get('/students', function (req, res){
+    let students = ['Sabiha', 'Neha', 'Akash']
+    res.send(students)
+})
 
-router.get('/test-you', function(req, res){
-    res.send('This is the second routes implementation')
-});
+router.get('/student-details/:name', function(req, res){
+    /*
+    params is an attribute inside request that contains 
+    dynamic values.
+    This value comes from the request url in the form of an 
+    object where key is the variable defined in code 
+    and value is what is sent in the request
+    */
 
-router.get('/give-me-students-data',function(req, res){
+    let requestParams = req.params
 
-});
-
-
+    // JSON strigify function helps to print an entire object
+    // We can use any ways to print an object in Javascript, JSON stringify is one of them
+    console.log("This is the request "+ JSON.stringify(requestParams))
+    let studentName = requestParams.name
+    console.log('Name of the student is ', studentName)
+    
+    res.send('Dummy response')
+})
 
 module.exports = router;
-// adding this comment for no reason
