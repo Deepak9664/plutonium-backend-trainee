13c5e4f1f92e65ad139364ce2698bedfa877c921
deepak9664 <singhdepak30@gmail.com>
2022-08-08 14:47:01 +0530

    external modules

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,19 +1,32 @@
 const express = require('express');
 const abc = require('../introduction/intro')
- const assign = require('../logger/logger')
- const assign1 = require('../util/helper')
- const assign2 = require('../validator/formatter')
+//  const assign = require('../logger/logger')
+//  const assign1 = require('../util/helper')
+//  const assign2 = require('../validator/formatter')
+const Lodash = require('lodash');
 const router = express.Router();
 
 router.get('/test-me', function (req, res) {
     console.log('My batch is plutonium', )
-    assign.Cuh()
-    assign1.F1()
-    assign1.F2()
-    assign1.F3()
-    assign2.Trim()
-    assign2.Smallalpha()
-    assign2.Bigalpha()
+    // assign.Cuh()
+    // assign1.F1()
+    // assign1.F2()
+    // assign1.F3()
+    // assign2.Trim()
+    // assign2.Smallalpha()
+    // assign2.Bigalpha()
+    const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul","Aug", "Sep", "Oct", "Nov", "Dec"];
+    const split = Lodash.chunk(months,4);
+    console.log(split);
+    const Odd = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19];
+    const Tail = Lodash.tail(Odd)
+    console.log(Tail);
+    const Arrayyy = Lodash.union([1,2,3],[3,4,5],[4,5,6],[5,6,7],[7,8,9]);
+    console.log(Arrayyy);
+    const Pairs = [["horror","The Shining"],["drama","Titanic"],["thriller","Shutter Island"],["fantasy","Pans Labyrinth"]]
+    const keypairs = Lodash.fromPairs(Pairs);
+    console.log(keypairs);
+
     res.send('My second ever api!')
 });
 
