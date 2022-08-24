cdc632339443ddd1a2cbb4c4c6555bf32cf7efbc
deepak9664 <singhdepak30@gmail.com>
2022-08-24 11:13:34 +0530

    fff

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-test:t6UXgvboja8qm5wn@cluster0.k6wrnuj.mongodb.net/test?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://Deepak9666:vwGhTuklxJu2hPPK@cluster0.l1wlrcl.mongodb.net/deepak9664", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
