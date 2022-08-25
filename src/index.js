dd24a4436134597248ecb704ba7209452454814a
deepak9664 <singhdepak30@gmail.com>
2022-08-25 23:57:10 +0530

    ddeee

--- src/index.js
+++ src/index.js
@@ -20,9 +20,24 @@ mongoose.connect("mongodb+srv://Deepak9666:vwGhTuklxJu2hPPK@cluster0.l1wlrcl.mon
 //         next();
 //   }
 //   );
-  
+// Write a middleware that logs (console.log) some data everytime any API is hit Data to be logged:-the current timestamp(as date time) , the IP of the user and the route being requested). For this first figure out how to get the route location being requested, how to get current timestamp and how to get the IP. NOTE: ip of local computer will come as ::1 so dont get disturbed by seeing this
 
-app.use('/', route);
+const moment = require("moment");
+const time = moment();
+app.use (
+    function (req, res, next) {
+        console.log ("inside GLOBAL MW");
+        console.log(time.format('YYYY,MM,DD'));
+        console.log(req.ip);
+        console.log(req.originalUrl);
+        next();
+            
+        res.send({msg:"done"})
+  }
+  );
+  app.use('/', route);
+
+
 
 
 app.listen(process.env.PORT || 3000, function () {
